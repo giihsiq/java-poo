522ca2221da84cdc55b6accd32c273510379394b
giihsiq <giihsiq.contato@gmail.com>
2026-07-24 23:12:13 -0300

    refactor: Ajustando anotações e melhorando a visualização do código da classe ProgramEmployee.java

--- src/com/udemy/devsuperior/list/Example.java
+++ src/com/udemy/devsuperior/list/Example.java
@@ -24,14 +24,14 @@ public class Example {
 		
 		System.out.println();
 		
-		// laço for each, cria uma variável e armazena o valor de list
+		// Laço for each, cria uma variável e armazena o valor de list - Imprimindo os valores armazenados na list
 		for (String x : list) {
 			System.out.println(x);
 		}
 		
 		System.out.println("--------------");
 		
-		// Remove o elemento fazendo a comparação, removendo caso esse valor exista
+		// Remove o elemento fazendo a comparação, e remove o valor inserido caso ele exista na lista
 		list.remove("Marley");
 		
 		// Expressão lambda/predicado, remove a letra na posição 0 se for igual a A
@@ -49,7 +49,7 @@ public class Example {
 		System.out.println("--------------");
 		
 		/* Criando nova lista para consumir a lista antiga
-		 * fazendo um stream() para filtar e buscar elementos que comecem com a letra G
+		 * fazendo um .stream() para filtar e buscar elementos que comecem com a letra G
 		   retornando isso para o collect, para consumir na nova lista e imprimir com o for each */
 		List<String> filter = list.stream().filter(x -> x.charAt(0) == 'G').collect(Collectors.toList());
 		for (String x : filter) {
--- src/com/udemy/devsuperior/list/exercicio/ProgramEmployee.java
+++ src/com/udemy/devsuperior/list/exercicio/ProgramEmployee.java
@@ -12,22 +12,24 @@ public class ProgramEmployee {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 		
-		int i;
-		
 		System.out.print("How many employees will be registered? ");
 		int n = scanner.nextInt();
 		
 		List<Employee> register = new ArrayList<>();
 		
-		for (i = 0; i < n; i++) {
-			System.out.println("Emplyoee #" + (i + 1));
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("Employee #" + (i + 1));
 			System.out.print("Id: ");
 			Integer id = scanner.nextInt();
+
 			/* Método estático hasId fazendo a validação para buscar se o id digitado já existe ou não */
 			while (hasId(register, id)) {
+
 				System.out.println("Id already taken, try again: ");
 				System.out.print("Id: ");
 				id = scanner.nextInt();
+
 			}
 			
 			System.out.print("Name: ");
@@ -39,31 +41,46 @@ public class ProgramEmployee {
 			
 			Employee employee = new Employee(id, name, salary);
 			register.add(employee);
+
 		}
 		
 		System.out.print("Enter the employee id that will have salary increase: ");
 		int id = scanner.nextInt();
+
+//		TODO: Método hasId()
 		Employee employee = register.stream().filter(x -> x.getId() == id).findAny().orElse(null);
+
 		if (employee != null) {
+
 			System.out.print("Enter the percentage: ");
 			double percentage = scanner.nextDouble();
+
 			employee.increaseSalary(percentage);
+
 		} else {
+
 			System.out.println("This id does not exist!");
+
 		}
 		
 		System.out.println();
+
 		System.out.println("List of employees:");
 		for (Employee emp : register) {
+
 			System.out.println(emp);
+
 		}
 		
 		scanner.close();
 	}
 	
 	public static boolean hasId(List<Employee> register, int id) {
+
 		Employee employee = register.stream().filter(x -> x.getId() == id).findAny().orElse(null);
+
 		return employee != null;
+
 	}
 
 }
